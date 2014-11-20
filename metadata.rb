10d63bac563ee43fcdff71a6cd51a2aa094f4fb5
Thiago Cardoso <thiagoncc@gmail.com>
2014-11-20 15:55:06 -0200

    Adding name to metadata
    Ridley::Errors::MissingNameAttribute The metadata at
    '/tmp/d20141120-16669-1kqff87' does not contain a 'name' attribute.
    While Chef does not strictly enforce this requirement, Ridley cannot
    continue without a valid metadata 'name' entry.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 maintainer       "JJ Asghar"
 maintainer_email "jj.asghar@peopleadmin.com"
+name             "riemann"
 license          "Apache 2.0"
 description      "Installs/Configures riemann client and server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
